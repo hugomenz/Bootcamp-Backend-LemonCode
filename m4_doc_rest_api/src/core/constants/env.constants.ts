337fc54f42634b84cbfb543da8a4b9c7e3bf238f
Hugo Martin <hugomartin.menz@gmail.com>
2024-05-10 12:50:16 +0200

    Refactor: moved express instance to rest-api.server.ts

---
+++ m4_doc_rest_api/src/core/constants/env.constants.ts
@@ -0,0 +1,6 @@
+export const envConstants = {
+  isProduction: process.env.NODE_ENV === "production",
+  PORT: process.env.PORT,
+  CORS_ORIGIN: process.env.CORS_ORIGIN,
+  CORS_METHODS: process.env.CORS_METHODS,
+};
--- m4_doc_rest_api/src/index.ts
+++ m4_doc_rest_api/src/index.ts
@@ -1,23 +1,25 @@
 import express from "express";
 import { listingsApi } from "./listings.api.js";
+import { createRestApiServer } from "./core/servers/index.js";
+import { envConstants } from "./core/constants/index.js";
+import "./core/load-env.js";
 
-const app = express();
-app.use(express.json());
+const restApiServer = createRestApiServer();
 
 // middleware log  method and req url
-app.use(async (req, res, next) => {
+restApiServer.use(async (req, res, next) => {
   console.log(`${req.method} ${req.url}`);
   next();
 });
 
-app.use("/api/listings", listingsApi);
+restApiServer.use("/api/listings", listingsApi);
 
 // middleware to  catch errors and log it by console
-app.use(async (error, req, res, next) => {
+restApiServer.use(async (error, req, res, next) => {
   console.error(error);
   res.sendStatus(500);
 });
 
-app.listen(3000, () => {
-  console.log("Server ready at port 3000");
+restApiServer.listen(envConstants.PORT, () => {
+  console.log(`Server ready at port ${envConstants.PORT}`);
 });
