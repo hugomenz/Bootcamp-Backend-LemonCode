22aabf95f540a69e3f53188e28a531bd2c02ff78
Hugo Martin <hugomartin.menz@gmail.com>
2024-05-10 16:17:28 +0200

    Removed comments

--- m4_doc_rest_api/src/dals/listing/mock-data.ts
+++ m4_doc_rest_api/src/dals/listing/mock-data.ts
@@ -7,10 +7,8 @@ export interface DB {
   listings: Listing[];
 }
 
-// Obtener el directorio actual a partir de import.meta.url
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-// Construyendo la ruta correcta al archivo JSON
 const jsonPath = path.resolve(__dirname, "./repositories/mock-data/listingsAndReviews.json");
 const listingsAndReviews = JSON.parse(fs.readFileSync(jsonPath, "utf-8")) as Listing[];
 
